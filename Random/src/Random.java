2d6f062b6a6cab302e8fd64010c52336270e6cc0
Wasi Mohammed Abdullah <wasi0013@gmail.com>
2015-03-12 01:45:37 +0600

    Update Random.java

--- Random/src/Random.java
+++ Random/src/Random.java
@@ -4,26 +4,26 @@ public class Random {
       static void random(){
 		int[] nums = new int[10];
 		for(int i=0; i<nums.length;i++){
-			int random= (int) (Math.random()*10);
-			boolean found=true;
+			int random = 1 + (int)(Math.random()*10);
+			boolean found = true;
 			
 			while(found){
-				random= (int) (Math.random()*10);
-				found=false;
+				random = 1 + (int)(Math.random()*10);
+				found = false;
 			
 				for (int num : nums) {
-					if (random==num) {
-						found=true;
+					if (random == num) {
+						found = true;
 						break;
 					}
 				}
 			}			
-			nums[i]=random;
+			nums[i] = random;
 			}
 			
 		//print the random number arrays
 		for(int num:nums){
-			System.out.print(num+" ");
+			System.out.print(num + " ");
 			
 		}
 		
@@ -31,8 +31,8 @@ public class Random {
 	}
 	public static void main(String[] args) {
 		
-		int counter=1;
-		while(counter<6){
+		int counter = 1;
+		while(counter < 6){
 		System.out.print("Random array of unique numbers Test ("+counter+"): ");
 			random();
 		System.out.println();
